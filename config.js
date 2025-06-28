4b251d1c87494945b5180bf79595bfdda7edde1a
Wassim Assidi <assidi.wassim.fsm@hotmail.com>
2025-06-28 18:36:34 +0100

    fix

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   stripeApiKey: "sk_test_51Hxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   dbPassword: "MySuperSecretPassword123!",
+  stripe:"sk_test_51Hxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 };
