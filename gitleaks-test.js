37022850e5c5ce329787f3eead0f619b7c9fb6b7
Wassim Assidi <assidi.wassim.fsm@hotmail.com>
2025-06-28 18:52:53 +0100

    fix

--- gitleaks-test.js
+++ gitleaks-test.js
@@ -1,4 +1,4 @@
 // This file intentionally contains a hardcoded secret for Gitleaks testing
-const secret = "sk_test_51H8QwL2eZvKYlo2C8QWbK6v1QwL2eZvKYlo2C8QWbK6v1QwL2eZvKYlo2C8QWbK6v1";
+const stripeSecret = "sk_test_51H8QwL2eZvKYlo2C8QWbK6v1QwL2eZvKYlo2C8QWbK6v1";
 
-export default secret;
+export default stripeSecret;
