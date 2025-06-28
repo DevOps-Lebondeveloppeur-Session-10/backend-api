5b39eb038fff570d293012951f9520e93f262c61
Wassim Assidi <assidi.wassim.fsm@hotmail.com>
2025-06-28 18:40:17 +0100

    Update test file with a Stripe key pattern for Gitleaks detection

--- gitleaks-test.js
+++ gitleaks-test.js
@@ -1,4 +1,4 @@
 // This file intentionally contains a hardcoded secret for Gitleaks testing
-const secret = "sk_test_51HFAKEgitleaksDETECTABLEKEY1234567890";
+const secret = "sk_test_51H8QwL2eZvKYlo2C8QWbK6v1QwL2eZvKYlo2C8QWbK6v1QwL2eZvKYlo2C8QWbK6v1";
 
 export default secret;
